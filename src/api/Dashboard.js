c790450ea9348b55e49f740e95a1dbcaa54ba116
konstruktiv <konstruktiv@yare-studio.com>
2024-02-02 20:22:39 +0300

    1.1.0 Global refactoring

--- src/api/Dashboard.js
+++ src/api/Dashboard.js
@@ -16,7 +16,7 @@ async function dashboard(cookie, key, removeCookies, navigate, setGuilds) {
                     if (guild.hasBot) {
                         navigate(`/g/${guild.id}`);
                     } else {
-                        window.location.replace(`https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&permissions=8&scope=bot+applications.commands&guild_id=${guild.id}&disable_guild_select=1&redirect_uri=${MAIN_REDIRECT_URI}/dashboard`);
+                        window.open(`https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&permissions=8&scope=bot+applications.commands&guild_id=${guild.id}&disable_guild_select=1&redirect_uri=${MAIN_REDIRECT_URI}/dashboard`);
                     }
                 }} logo={guild.icon != null ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png` : replace} label={guild.name} notHaveBot={!guild.hasBot ? true : false} key={guild.id} />])
             }
